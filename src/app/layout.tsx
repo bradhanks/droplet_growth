58e864c62bb667177f5a286ae0854f642c631a62
Brad Hanks <hanks.brad@gmail.com>
2025-07-18 20:16:24 -0600

    text changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,10 +2,10 @@ import { type Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import localFont from 'next/font/local'
 import clsx from 'clsx'
+
 import { Providers } from '@/app/providers'
 import { Layout } from '@/components/Layout'
-// import diamonds from '@/images/diamonds.svg'
-// import Image from 'next/image'
+
 import '@/styles/tailwind.css'
 
 const inter = Inter({
@@ -23,11 +23,11 @@ const lexend = localFont({
 
 export const metadata: Metadata = {
   title: {
-    template: '%s - prepared by SeriesLab.',
-    default: 'droplet.io - Growth Proposal.',
+    template: '%s - Docs',
+    default: 'CacheAdvance - Never miss the cache again.',
   },
   description:
-    'Let\'s gooooo!',
+    'Cache every single thing your app could ever do ahead of time, so your code never even has to run at all.',
 }
 
 export default function RootLayout({
@@ -43,14 +43,6 @@ export default function RootLayout({
     >
       <body className="flex min-h-full bg-white dark:bg-slate-900">
         <Providers>
-          {/* <Image
-            alt="Background"
-            src={diamonds}
-            className={clsx("fixed max-w-none top-[16.6em] left-[7.8vw] opactity-10")}
-            width={2100}
-            height={1000}
-            priority
-          /> */}
           <Layout>{children}</Layout>
         </Providers>
       </body>
